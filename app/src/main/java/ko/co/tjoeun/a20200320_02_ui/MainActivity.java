fbbb837ba6b822555059f1d9d1e58b76283c6bbc
khu12687 <61855040+khu12687@users.noreply.github.com>
2020-03-16 15:52:51 +0900

    Initial commit

---
+++ app/src/main/java/ko/co/tjoeun/a20200320_02_ui/MainActivity.java
@@ -0,0 +1,14 @@
+package ko.co.tjoeun.a20200320_02_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
